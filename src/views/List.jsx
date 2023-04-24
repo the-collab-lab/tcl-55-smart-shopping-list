c72ba752a859c195889adf33b453724bfb69b771
Ticia Francisco <ticiadev@gmail.com>
2023-04-24 16:52:15 -0700

    Add empty list prompt

--- src/views/List.jsx
+++ src/views/List.jsx
@@ -1,7 +1,10 @@
 import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { ListItem } from '../components';
 
 export function List({ data }) {
+	const navigate = useNavigate();
+
 	const [searchInput, setSearchInput] = useState('');
 	const [filteredData, setFilteredData] = useState([]);
 
@@ -26,27 +29,48 @@ export function List({ data }) {
 
 	const handleFormSubmit = (e) => e.preventDefault();
 
+	const handleFirstItem = () => {
+		navigate('/add-item');
+	};
+
 	return (
 		<>
-			<form
-				onSubmit={handleFormSubmit}
-				style={{ display: 'flex', gap: '1rem' }}
-			>
-				<label htmlFor="search">Filter Items</label>
-				<input
-					type="text"
-					id="search"
-					name="search"
-					value={searchInput}
-					onChange={handleSearchInput}
-					placeholder="Start typing here..."
-				></input>
-				{searchInput.length > 0 ? (
-					<button type="reset" name="clear" onClick={handleClear}>
-						Clear Filter
+			{data.length === 0 && (
+				<section
+					style={{
+						display: 'flex',
+						alignItems: 'center',
+						flexDirection: 'column',
+					}}
+				>
+					<p>Your shopping list is currently empty.</p>
+					<button name="firstItem" onClick={handleFirstItem}>
+						Add Item
 					</button>
-				) : null}
-			</form>
+				</section>
+			)}
+
+			{data.length !== 0 && (
+				<form
+					onSubmit={handleFormSubmit}
+					style={{ display: 'flex', gap: '1rem' }}
+				>
+					<label htmlFor="search">Filter Items</label>
+					<input
+						type="text"
+						id="search"
+						name="search"
+						value={searchInput}
+						onChange={handleSearchInput}
+						placeholder="Start typing here..."
+					></input>
+					{searchInput.length > 0 ? (
+						<button type="reset" name="clear" onClick={handleClear}>
+							Clear Filter
+						</button>
+					) : null}
+				</form>
+			)}
 			<ul>
 				{filteredData.map((item) => (
 					<ListItem key={item.id} name={item.name} />
