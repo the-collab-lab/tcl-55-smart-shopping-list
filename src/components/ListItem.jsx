1230f505cbd0d2bcdb2887b6a27e83fa91492fd6
Ticia Francisco <ticiadev@gmail.com>
2023-05-08 16:43:07 -0700

    Start details feature

--- src/components/ListItem.jsx
+++ src/components/ListItem.jsx
@@ -1,9 +1,15 @@
+import { useState } from 'react';
 import { updateItem } from '../api/firebase';
 import { isWithinLastDay } from '../utils/dates';
 import './ListItem.css';
 
 export function ListItem({ item, listId, handleDeleteItem }) {
-	const { id, name } = item;
+	const [showDetails, setShowDetails] = useState(false);
+	const { id, name, totalPurchases } = item;
+
+	//TODO: get purchase date data from firestore
+	/* const lastPurchasedDate = item.dateLastPurchased.toDate();
+	const nextPurchasedDate = item.dateNextPurchased.toDate(); */
 
 	const handlePurchase = async (e) => {
 		if (e.target.checked) {
@@ -11,24 +17,46 @@ export function ListItem({ item, listId, handleDeleteItem }) {
 		}
 	};
 
+	const handleShowDetails = () => {
+		setShowDetails(!showDetails);
+		console.log(item);
+	};
+
 	return (
-		<li className="ListItem">
-			<input
-				type="checkbox"
-				id={`mark-${name}-purchased-id-${id}`}
-				onChange={handlePurchase}
-				checked={isWithinLastDay(item.dateLastPurchased)}
-			/>
-			<label htmlFor={`mark-${name}-purchased-id-${id}`}>{name}</label>
-			<button
-				type="button"
-				name="delete"
-				onClick={handleDeleteItem}
-				value={item.id}
-				style={{ marginLeft: '.5rem' }}
-			>
-				Delete
-			</button>
-		</li>
+		<>
+			<li className="ListItem">
+				<input
+					type="checkbox"
+					id={`mark-${name}-purchased-id-${id}`}
+					onChange={handlePurchase}
+					checked={isWithinLastDay(item.dateLastPurchased)}
+				/>
+				<label htmlFor={`mark-${name}-purchased-id-${id}`}>{name}</label>
+				<button
+					type="button"
+					name="details"
+					onClick={handleShowDetails}
+					style={{ marginLeft: '.5rem' }}
+				>
+					Details
+				</button>
+				<button
+					type="button"
+					name="delete"
+					onClick={handleDeleteItem}
+					value={item.id}
+					style={{ marginLeft: '.5rem' }}
+				>
+					Delete
+				</button>
+			</li>
+			{showDetails && (
+				<div>
+					{/* <p>Last purchased: {dateLastPurchased}</p>
+					<p>Next purchased: {dateNextPurchased}</p> */}
+					<p>You have purchased this item {totalPurchases} times</p>
+				</div>
+			)}
+		</>
 	);
 }
